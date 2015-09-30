1f20269b8ce7a76221ee34c3f88d5cbd8bd65daf
raphaelsimpelo <rsimpelo@gmail.com>
2015-09-30 13:30:38 -0700

    [Deleted] MySQL and Bookshelf ORM

--- server/collections/Pets.js
+++
@@ -1,8 +0,0 @@
-var db = require('../schema.js');
-var Pet = require('../models/Pet.js');
-
-var Pets = new db.Collection();
-
-Pets.model = Pet;
-
-module.exports = Pets;
--- server/collections/Sitters.js
+++
@@ -1,8 +0,0 @@
-var db = require('../schema.js');
-var Sitter = require('../models/Sitter.js');
-
-var Sitters = new db.Collection();
-
-Sitters.model = Sitter;
-
-module.exports = Sitters;
