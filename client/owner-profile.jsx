d781f655dd8f7c75721c3eb219ba0478d6167cb8
Android <plasm0dium@Androids-MacBook-Pro.local>
2015-09-29 18:08:47 -0700

    [Progress] main file successfully made

--- client/owner-profile.jsx
+++ client/owner-profile.jsx
@@ -6,35 +6,27 @@ var React = require('react');
 module.exports = React.createClass({
 	render: function(){
 		return (
-			<div>
-        {this.renderPetProfile()}
-      </div>
       <div>
+        {this.renderPetProfile()}
         {this.renderOwnerProfile()}
-			</div>
+      </div>
+      
 		)
 	},
   renderOwnerProfile: function(){
-    render: function(){
-      return ( //this.state.(ownersInfo).map(function(owner){})
-        <div>
+      return (
+        <div> //this.state.(ownersInfo).map(function(owner){})
           <h4>{this.props.owners.firstname} + " " + {this.props.owners.lastname} </h4>
         </div>
         )
-    }
-
   },
   renderPetProfile: function(){
-    render: function() {
       return ( //this.state.(petsInfo).map(function(pet)) {})
         <div>
           <h6>{this.props.pets.name} + " " + {this.props.pets.size} + " " + {this.props.pets.species}</h6>
         </div>
-
-        )
-    }
-
-  },
+      )
+  }
 
   // onChange: function(){
 
